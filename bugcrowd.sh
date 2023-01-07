d107bbb31378a70169f1101dceffa25f26655e8c
Ruben <rubenskywalker44@gmail.com>
2023-01-07 22:44:52 +0200

    Update bugcrowd.sh

--- bugcrowd.sh
+++ bugcrowd.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-#Script to automatically check for bugs on bugcrowd for all companies listed on their website
-#Set target URL
-target_url="https://www.bugcrowd.com"
+#Script to automatically check for bugs on bugcrowd
+#Prompt user for target URL
+echo "Enter target URL:"
+read target_url
 
 #Create .txt file to store vulnerabilities
 touch vulnerabilities.txt
 
-#Retrieve list of all companies on bugcrowd website
-companies=$(curl -s $target_url | grep -o 'companies/.*">' | sed 's/companies///g;s/">//g')
+#Run initial scan with nikto tool
+nikto -h $target_url >> vulnerabilities.txt
 
-#Loop through each company and run scans
-for company in $companies; do
-echo "Scanning $company..."
-nikto -h $target_url/companies/$company >> vulnerabilities.txt
-w3af -t $target_url/companies/$company >> vulnerabilities.txt
-burpsuite -t $target_url/companies/$company >> vulnerabilities.txt
-done
+#Run second scan with w3af tool
+w3af -t $target_url --audit xss --audit sql >> vulnerabilities.txt
+
+#Run final scan with Burp Suite
+burpsuite -t $target_url >> vulnerabilities.txt
 
 #Print results of scans
 echo "Scan complete. Vulnerabilities saved in vulnerabilities.txt."
